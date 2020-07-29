cfad37e25dd9e8576163f0173a34d3208b0a2b4a
Akhil Potla <akhil4400@gmail.com>
2020-07-29 15:59:09 -0500

    Backend and frontend can communicate

--- app/src/pages/App.tsx
+++ app/src/pages/App.tsx
@@ -1,14 +1,24 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import {navigate, Router} from "@reach/router"
 import styles from "./App.module.scss"
 import Home from "./Home";
 
 
 export default function App() {
+
+  const [apiResponse, setAPIResponse] = useState('');
+
+  useEffect(() => {
+      fetch('http://localhost:9000/testAPI').then(res => res.text()).then(data => {
+          setAPIResponse(data)
+      })
+  }, [])
+
   return (
     <>
       <header>
         <h1 onClick={() => navigate("/")}>Fuzzydoro</h1>
+        <p>{apiResponse}</p>
       </header>
       <div className={styles.main}>
         <Router>
