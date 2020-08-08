a2cda4faffec97892939190f90d030cd98ca620f
Akhil Potla <akhil4400@gmail.com>
2020-08-08 13:14:58 -0500

    Removed testing api call

--- app/src/pages/App.tsx
+++ app/src/pages/App.tsx
@@ -1,25 +1,17 @@
-import axios from "axios"
-import React, { useState, useEffect } from "react"
+import React from "react"
 import {navigate, Router} from "@reach/router"
 import styles from "./App.module.scss"
 import Home from "./Home";
 
+import Timer from "../components/Timer"
 
 export default function App() {
-
-  const [apiResponse, setAPIResponse] = useState('');
-
-  useEffect(() => {
-      axios.get('/testAPI').then(res => {
-          setAPIResponse(res.data)
-      })
-  }, [])
 
   return (
     <>
       <header>
         <h1 onClick={() => navigate("/")}>Fuzzydoro</h1>
-        <p>{apiResponse}</p>
+        <Timer />
       </header>
       <div className={styles.main}>
         <Router>
