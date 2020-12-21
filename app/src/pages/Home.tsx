08316c1b49bc2122c2ddcd0a274e6a9f9843ba3e
Akhil Potla <akhil4400@gmail.com>
2020-12-21 11:31:59 -0600

    Margin on landing page

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,10 +1,24 @@
 import React from "react"
 import {RouteComponentProps} from "@reach/router"
+import Button from "react-bootstrap/Button"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+
+import styles from "./Home.module.scss"
 
 type Props = RouteComponentProps
 
 function Home(props: Props) {
-  return <div></div>
+  return (
+    <Container  className={styles["container-margin"]}fluid>
+      <Row className="justify-content-center">
+        <h1>Making Time Blocking Work For You</h1>
+      </Row>
+      <Row className="justify-content-center">
+        <Button className={styles["button-margin"]} variant="danger" href="/timer"><h4>Try it Now!</h4></Button>
+      </Row>
+    </Container>
+  )
 }
 
 
